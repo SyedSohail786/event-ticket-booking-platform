7a857b3a9b9883f1f221a9832d7f00294e73191d
Syed Sohail <sohailsyed7867123@gmail.com>
2025-07-12 13:41:43 +0530

    added info

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true }, // ✅ added
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePic: { type: String, default: 'https://t4.ftcdn.net/jpg/06/22/22/17/360_F_622221708_Gg16ZdaNSixeaIORq9MuuT4w9VWTkYw4.jpg' },
+    profilePic: { type: String, default: '' },
   },
   { timestamps: true }
 );
